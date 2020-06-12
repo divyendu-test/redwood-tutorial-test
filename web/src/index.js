12d9332e72aceb759e1e3761bbeec5928b3ed394
David S Price <thedavid@thedavidprice.com>
2020-06-11 21:49:34 -0700

    use Auth0

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from '@redwoodjs/auth'
-import netlifyIdentity from 'netlify-identity-widget'
+import { Auth0Client } from '@auth0/auth0-spa-js'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -9,11 +9,17 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
-netlifyIdentity.init()
+const auth0 = new Auth0Client({
+  domain: process.env.AUTH0_DOMAIN,
+  client_id: process.env.AUTH0_CLIENT_ID,
+  redirect_uri: 'http://localhost:8910/',
+  cacheLocation: 'localstorage',
+  audience: process.env.AUTH0_AUDIENCE,
+})
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <AuthProvider client={netlifyIdentity} type="netlify">
+    <AuthProvider client={auth0} type="auth0">
       <RedwoodProvider>
         <Routes />
       </RedwoodProvider>
