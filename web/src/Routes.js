7c9acd53a995c0ff9ca3f4d07f21ddfe6385cd07
David S Price <thedavid@thedavidprice.com>
2020-08-04 13:16:59 -0700

    remove private routes for testing

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,16 +14,16 @@ const Routes = () => {
     <Router>
       <Route path="/contact" page={ContactPage} name="contact" />
       <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-      <Private unauthenticated="home">
-        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-        <Route
-          path="/admin/posts/{id:Int}/edit"
-          page={EditPostPage}
-          name="editPost"
-        />
-        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-        <Route path="/admin/posts" page={PostsPage} name="posts" />
-      </Private>
+      {/* <Private unauthenticated="home"> */}
+      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+      <Route
+        path="/admin/posts/{id:Int}/edit"
+        page={EditPostPage}
+        name="editPost"
+      />
+      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/admin/posts" page={PostsPage} name="posts" />
+      {/* </Private> */}
       <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
